f284335b6b5f63cb866a2f8a1e0ca88bda379fdf
bxq2011hust <bxq2011hust@qq.com>
2021-07-19 17:37:15 +0800

    add intergation test

---
+++ src/integration-test/java/org/fisco/bcos/sdk/transaction/mock/RemoteSignCallbackMock.java
@@ -0,0 +1,48 @@
+package org.fisco.bcos.sdk.transaction.mock;
+
+import org.fisco.bcos.sdk.crypto.signature.SignatureResult;
+import org.fisco.bcos.sdk.model.TransactionReceipt;
+import org.fisco.bcos.sdk.transaction.manager.AssembleTransactionWithRemoteSignProcessor;
+import org.fisco.bcos.sdk.transaction.model.po.TransactionData;
+import org.fisco.bcos.sdk.transaction.signer.RemoteSignCallbackInterface;
+import org.fisco.bcos.sdk.transaction.tools.JsonUtils;
+
+import java.util.concurrent.ExecutionException;
+
+public class RemoteSignCallbackMock implements RemoteSignCallbackInterface {
+
+    protected AssembleTransactionWithRemoteSignProcessor assembleTransactionWithRemoteSignProcessor;
+    protected TransactionData rawTransaction;
+
+    public RemoteSignCallbackMock(
+            AssembleTransactionWithRemoteSignProcessor assembleTransactionWithRemoteSignProcessor,
+            TransactionData rawTransaction) {
+        this.assembleTransactionWithRemoteSignProcessor =
+                assembleTransactionWithRemoteSignProcessor;
+        this.rawTransaction = rawTransaction;
+    }
+
+    /**
+     * 签名结果回调的实现
+     *
+     * @param signatureStr 签名服务回调返回的签名结果串
+     * @return *
+     */
+    @Override
+    public int handleSignedTransaction(SignatureResult signatureStr) {
+        System.out.println(System.currentTimeMillis() + " SignatureResult: " + signatureStr);
+        // 完成了交易签名后，将其发送出去
+        TransactionReceipt tr = null;
+        try {
+            tr = assembleTransactionWithRemoteSignProcessor.signAndPush(
+                    rawTransaction, signatureStr.getSignatureBytes()).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println(
+                "handleSignedTransaction transactionReceipt is: " + JsonUtils.toJson(tr));
+        return 0;
+    }
+}
